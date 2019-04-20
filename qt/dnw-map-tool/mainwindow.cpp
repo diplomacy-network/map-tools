4b1b4cd9e286ed37051386c5bcb0d5440dfad9de
Alexander Wulf <dev@alexfwulf.de>
2019-04-20 16:49:55 +0200

    QT-Development moved

--- qt/dnw-map-tool/mainwindow.cpp
+++ qt/dnw-map-tool/mainwindow.cpp
@@ -6,9 +6,31 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //TiletypeList
+    tiletypeListModel = new QStringListModel(this);
+
+    //QStringList tiletypeList;
+
+
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on_addTiletypeButton_clicked()
+{
+
+}
+
+void MainWindow::on_editTiletypeButton_clicked()
+{
+
+}
+
+void MainWindow::on_removeTiletypeButton_clicked()
+{
+
 }
