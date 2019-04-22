c3b6ae4c4dfa9983d55f70ccc60ec8ff21cf587c
Alexander Wulf <dev@alexfwulf.de>
2019-04-22 22:58:37 +0200

    retrieveProvincePaths started

--- modules/svgHandler.js
+++ modules/svgHandler.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const parser = require('fast-xml-parser');
-const jmespath = require('jmespath');
+var jsonata = require("jsonata");
 
 const inspect = require('eyes').inspector({
     maxLength: false
@@ -9,6 +9,11 @@ const inspect = require('eyes').inspector({
 
 
 module.exports = {
+    /**
+     * Parses a XML file to a json file
+     *
+     * @returns json file as defined under dnw-planning/mapTools/xml-parsed.json
+     */
     parseXmlToJson: function () {
         file = fs.readFileSync(__dirname + '/MapTest.svg')
         xmlData = file.toString();
@@ -26,13 +31,47 @@ module.exports = {
         }
         return parsed;
     },
-
+    
     checkIfAllProvinceIDsAreUnique: function (parsed) {
         //Get a list of all provinceIDs
-        expression = "svg.g[?id=='tiles'].g[].path[].id"
-        result = jmespath.search(parsed, expression)
+        expression = jsonata("svg.g[id='tiles'].g.path.id");
+        result = expression.evaluate(parsed)
         // inspect(result)
         return (new Set(result).size === result.length);
+    },
+
+    retrieveProvincePaths: function (parsed) {
+        expression = jsonata("svg.g[id = 'tiles'].g");
+        result = expression.evaluate(parsed);
+        var stagedHandled = {provinces: []};
+        
+        result.forEach(tasu => {
+            currentTiletype = tasu.id;
+            
+            tasu.path.forEach(finn => {
+                if(finn.id.includes("union_")) {
+                    //UNION Handler
+                    current = {
+                        short: finn.id,
+                        tiletype: currentTiletype,
+                        provinceOutlinePath: finn.d,
+                    }
+                    
+                } else {
+                    stagedHandled.provinces.push({
+                        short: finn.id,
+                        tiletype: currentTiletype,
+                        provinceOutlinePath: finn.d,
+                        unionParts: [{
+                            id: "main",
+                            unionPartPath: finn.d
+                        }]
+                    })
+                }
+                
+            });
+        });
+        return stagedHandled
     }
 }
 
--- test.js
+++ test.js
@@ -1,10 +1,14 @@
 const inspect = require('eyes').inspector({
     maxLength: false
 });
+const fs = require('fs')
 
 const svgHandler = require ('./modules/svgHandler.js')
 
 let ret = svgHandler.parseXmlToJson()
+fs.writeFileSync("./xmlParsed.json", JSON.stringify(ret))
 // inspect(ret)
 unique = svgHandler.checkIfAllProvinceIDsAreUnique(ret);
 console.log(unique)
+let returnvalue = svgHandler.retrieveProvincePaths(ret)
+inspect(returnvalue)
